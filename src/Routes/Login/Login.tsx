7ad46f877e47bd3a850f0c3ac0bb52bf4de23e43
Ellie <elliecrome94@gmail.com>
2022-09-08 18:38:00 +0100

    Basic styling on login page

--- src/Routes/Login/Login.tsx
+++ src/Routes/Login/Login.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Link from "@mui/material/Link";
+import "./login.css";
 
 const Login = () => {
-  return <a href="/auth/login">Login with Spotify</a>;
+  return (
+    <div className="page">
+      <Link href="/auth/login" variant="h3">
+        Login with Spotify
+      </Link>
+    </div>
+  );
 };
 
 export { Login };
